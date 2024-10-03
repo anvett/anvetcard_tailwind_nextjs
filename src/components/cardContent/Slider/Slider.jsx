849e709cbd31c75adc91b0c610b6dbc094e28621
Andres Veintimilla <andveti@gmail>
2024-10-03 18:58:02 -0500

    Slider component adjusted

--- src/components/cardContent/Slider/Slider.jsx
+++ src/components/cardContent/Slider/Slider.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+
+
+import React, { useRef } from 'react';
 
 const Slider = ({ slides }) => {
+  const carouselRef = useRef(null);
+
+  const handleScrollToSlide = (index) => {
+    const slideElement = document.querySelector(`#slide${index}`);
+    if (slideElement) {
+      carouselRef.current.scrollTo({
+        left: slideElement.offsetLeft,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <div className="container mx-auto px-4">
-      <div className="carousel w-full">
+      {/* Carousel container with scroll snapping */}
+      <div
+        ref={carouselRef}
+        className="carousel w-full overflow-x-scroll snap-x snap-mandatory scroll-smooth"
+      >
         {slides.map((slide, index) => (
-          <div key={index} id={`slide${index}`} className="carousel-item relative w-full">
-            <img src={slide.image} className="w-full" alt={`Slide ${index}`} />
+          <div
+            key={index}
+            id={`slide${index}`}
+            className="carousel-item relative w-full snap-start"
+          >
+            {/* Imagen del carrusel */}
+            <img
+              src={slide.image}
+              className="w-full lg:w-4/5 mx-auto" // 80% en pantallas grandes
+              alt={`Slide ${index}`}
+            />
           </div>
         ))}
       </div>
-      <div className="text-center mt-spacing-2 flex flex-wrap sm:mx-spacing-3 gap-2 justify-center  ">
+
+      {/* Botones para cambiar de imagen */}
+      <div className="text-center mt-spacing-2 flex flex-wrap sm:mx-spacing-3 gap-2 justify-center">
         {slides.map((slide, index) => (
-          <button key={index} aria-label={`Slide ${index}`} className={`btn slide-button ${index === 0 ? 'btn-active' : ''}`} onClick={() => document.querySelector(`#slide${index}`).scrollIntoView({ behavior: 'smooth' })}>{slide.title}</button>
+          <button
+            key={index}
+            aria-label={`Slide ${index}`}
+            className={`btn slide-button ${
+              index === 0 ? 'btn-active' : ''
+            }`}
+            onClick={() => handleScrollToSlide(index)}
+          >
+            {slide.title}
+          </button>
         ))}
       </div>
     </div>
